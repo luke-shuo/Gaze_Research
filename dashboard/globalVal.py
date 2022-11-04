fbcd48f2f73218fb354def92374a73209a399a98
Zhang Shuo <2819601S@student.gla.ac.uk>
2022-11-04 12:16:52 +0000

    Update globalVal.py

--- dashboard/globalVal.py
+++ dashboard/globalVal.py
@@ -1,7 +1,9 @@
+
 # globalVal.py
 # Used to store global value
 
 # Data address
+
 gaze_data = '/Users/2602651K/Documents/GitHub/Gaze_Research/dashboard/dataset.csv'
 image_data = '/Users/2602651K/Documents/GitHub/Gaze_Research/dashboard/image_data/s1.png'
 video_data = '/Users/2602651K/Documents/GitHub/Gaze_Research/dashboard/video/'
